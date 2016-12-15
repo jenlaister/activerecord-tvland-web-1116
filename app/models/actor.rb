76ac572f472df9ad078f9e2f006705b12bbcf37d
Jenny Lai <jenny.lai@flatironschool.com>
2016-12-15 00:47:57 -0500

    Done

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,13 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+   "#{Character.pluck(:name).first} - #{Show.pluck(:name).first}"
+end
+
 end
